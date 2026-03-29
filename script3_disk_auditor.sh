68cd3c3ed0a4e4851eec694847fa803405631c7b
vijaynmishra <vijay.24bai10346@vitbhopal.ac.in>
2026-03-29 23:23:48 +0530

    Update script3_disk_auditor.sh

--- script3_disk_auditor.sh
+++ script3_disk_auditor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script 3: Disk and Permission Auditor
-# Author: Bhav Simar
+# Author: Vijay Naveen Mishra
 # Course: Open Source Software
 
 DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
